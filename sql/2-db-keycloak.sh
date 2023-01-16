9d0800ef5edf6c70bb777f12fb2b7e8cabf48bad
edovinivan <79606635949@yandex.ru>
2023-01-16 10:01:07 +0300

    Файлы sh

--- sql/2-db-keycloak.sh
+++ sql/2-db-keycloak.sh
@@ -2,4 +2,5 @@
 
 psql -U dev -tc "SELECT 1 FROM pg_database WHERE datname = 'keycloak'" | grep -q 1 || psql -U dev -c "CREATE DATABASE keycloak"
 
+
 psql -U dev -c "CREATE USER keycloak WITH PASSWORD 'kc'"
